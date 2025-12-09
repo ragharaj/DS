9ed9ebeced21a700a265bbc26e052039ffd532e6
Raghavendra Raj <ragha@RaghavendrasAir.lan>
2025-12-09 21:57:40 +0000

    LL Complete

--- LinkedList.cs
+++ LinkedList.cs
@@ -20,6 +20,38 @@ public class LinkedList
     head = newNode;
     
   }
+
+  void insertAtEnd (int data)
+  {
+    Node newNode = new Node (data);
+    Node temp = head;
+    if (head == null)
+    {
+      insertAtBeginning(data);
+      return;
+    }
+    while(temp.next != null)
+    {
+      temp = temp.next;
+    }
+    temp.next = newNode;
+  }
+  void insertAtIndex( int index, int data)
+  {
+    if (index == 0)
+    {
+      insertAtBeginning(data);
+      return;
+    }
+    Node newNode = new Node(data);
+    Node temp = head;
+    for (int i = 0; i < index - 1; i++)
+    {
+      temp = temp.next;
+    }
+    newNode.next = temp.next;;
+    temp.next = newNode;  
+  }
   void display()
   {
     Node? temp = head;
@@ -37,5 +69,13 @@ public class LinkedList
     ll.insertAtBeginning(20);
     ll.insertAtBeginning(30);
     ll.display();
+
+    ll.insertAtEnd(5);
+    ll.display();
+
+    ll.insertAtIndex(2, 15);
+    ll.display();
+
+
   }
 }
